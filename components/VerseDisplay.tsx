09fe28b345f9c333bc75c9e61f32fa95f5606814
Herb Segismar <herbert.segismar@yahoo.com>
2025-11-26 00:13:36 +1300

    added this space before encircled letters to fix clickable area, {" "}{part}

--- components/VerseDisplay.tsx
+++ components/VerseDisplay.tsx
@@ -150,7 +150,7 @@ const renderTree = (
                   lineHeight: baseFontSize * 1.4, // Maintain line height consistency
                 }}
               >
-                {part}
+                {" "}{part}
               </Text>
             );
           } else if (part) {
